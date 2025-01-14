f10809479fe0643307751e2f6a08327855679673
palw3ey <palw3ey@gmail.com>
2025-01-13 21:54:41 -0300

    Add files via upload

---
+++ tests/test.py
@@ -0,0 +1,37 @@
+
+import os
+import unittest
+import subprocess
+
+ENDPOINT_URL = os.getenv('URL')
+AWS_ACCESS_KEY_ID = os.getenv('KEY')
+AWS_SECRET_ACCESS_KEY = os.getenv('SECRET')
+HREF_BASE_URL = os.getenv('BASE')
+ARGUMENTS = os.getenv('ARGUMENTS')
+        
+class TestExampleScript(unittest.TestCase):
+    def test_script_output(self):
+        # Run the script using subprocess
+        result = subprocess.run(
+            ["python", "src/ye3sld/ye3sld.py",
+             "--endpoint_url", ENDPOINT_URL,
+             "--aws_access_key_id", AWS_ACCESS_KEY_ID,
+             "--aws_secret_access_key", AWS_SECRET_ACCESS_KEY,
+             "--href_base_url", HREF_BASE_URL,
+             ARGUMENTS,
+            ],
+            capture_output=True,
+            text=True
+        )
+        
+        # Print the output for debugging
+        print("Script output:", result.stdout.strip())
+        
+        # Check if the output starts with "Success"
+        self.assertTrue(result.stdout.strip().startswith("Success"))
+        
+        # Optionally, check if there were no errors
+        self.assertEqual(result.returncode, 0)
+
+if __name__ == "__main__":
+    unittest.main()
